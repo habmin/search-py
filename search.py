f0fc85189eec5c4e0aa21b23fa77bbe1773797a2
habmin <henryandrewbaum@gmail.com>
2023-06-23 16:59:29 -0400

    Added argparse for path parameter

--- search.py
+++ search.py
@@ -1,4 +1,16 @@
 import re
 import sys
 import pathlib
+import os
+import argparse
 
+def driver(*args, **kwargs):
+    parser = argparse.ArgumentParser(description = "Find matching string in directory/files and output where they're found into markdown")
+    parser.add_argument(dest='path', type = lambda p: pathlib.Path(p).absolute())
+    
+    args = parser.parse_args()
+
+    print(args.path)
+
+if __name__ == '__main__':
+    driver(*sys.argv)
