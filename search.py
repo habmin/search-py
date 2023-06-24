19e51b42e3fd0409f37e4c4e2cc1d92a8717e0ed
habmin <henryandrewbaum@gmail.com>
2023-06-24 04:03:11 -0400

    parse through blobs and some exceptions

--- search.py
+++ search.py
@@ -4,6 +4,9 @@ import pathlib
 import os
 import argparse
 
+REG_EX=r"created_at"
+exceptions = [".git", "data", "migrations", "diagrams", "docker", "fixture", "node_modules", "vendor", "storage"]
+
 # Checks if input is a valid directory and converts to posix.path
 def abs_path_check(string_input):
     if os.path.isdir(string_input):
@@ -11,16 +14,25 @@ def abs_path_check(string_input):
     else:
         raise NotADirectoryError(string_input)
 
-def parse_files(root_dir, indent):
+def parse_files(root_dir):
     for child in os.scandir(root_dir):
-        if child.name == ".git":
+        if child.name in exceptions:
             continue
         if child.is_dir():
-            print(indent, "* Dirc: ", child.path)
-            indent += " "
-            parse_files(child.path, indent)
+            # print(indent, "* Dirc: ", child.path)
+            parse_files(child.path)
         else:
-            print(indent, "- File: ", child.path)
+            # print("- File: ", child.path)
+            try:
+                file = open(child.path, "r")
+                for i, line in enumerate(file):
+                    if re.search(REG_EX, line):
+                        print(f"In file {child.path}")
+                        print(f"{i}: {line}")
+            except UnicodeDecodeError:
+                pass
+
+
 
 def driver(*args, **kwargs):
     parser = argparse.ArgumentParser(description = "Find matching string in directory/files and output where they're found into markdown")
@@ -28,7 +40,7 @@ def driver(*args, **kwargs):
     
     args = parser.parse_args()
 
-    parse_files(args.path, "")
+    parse_files(args.path)
 
 if __name__ == '__main__':
     driver(*sys.argv)
